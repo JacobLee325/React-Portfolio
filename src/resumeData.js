05e7c81177631d54200db31d84441ae8e0a5bdb8
JacobLee325 <jacob.doordie@gmail.com>
2021-03-10 09:37:52 -0800

    JacobLee React Based Portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,94 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Jacob Lee",
+    "role": "Python, Machine Learning Expert,  Full-Stack Web Developer",
+    "skypeid": "Jacob Lee",
+    "roleDescription": "I am a Full Stack web developer and Software Engineer with Expert-Level Skills and 8+ years experiences in programming.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/passive-doordie-a89054208/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/jacobLee325",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://skype.com/jacob Lee",
+          "className":"fa fa-skype"
+        }
+      ],
+    "aboutme":"I am a Full-Stack web developer and Software Engineer. I have 8 + years experiences.",
+    "address":"Windsor, Ontario, Canada",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"University Of Windsor",
+        "specialization":"Software Engineering",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2014",
+        "Achievements":"Bachelor of Degree"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"1718cafe.com",
+        "specialization":"Co-Developer",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2020",
+        "Achievements":"wordpress, woocommerce"
+      }
+    ],
+    "skillsDescription":"My Expert-Level skills are:",
+    "skills":[
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Vuejs"
+      },
+      {
+        "skillname":"MERN"
+      },
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascipt"
+      },
+      {
+        "skillname":"PHP"
+      }
+
+    ],
+    "portfolio":[
+      {
+        "name":"1718cafe.com",
+        "description":"1718cafe.com",
+        "imgurl":"images/portfolio/cafe.png"
+      },
+      {
+        "name":"project2",
+        "description":"https://www.metalflakecity.com/",
+        "imgurl":"images/portfolio/metal_flack.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Jacob Lee is creative and passion developer with expert-level skills and he always founded best solutions for my company",
+        "name":"Mohamed H"
+      },
+      {
+        "description":"His clean, qualified and robust code results made me happy",
+        "name":"Alena"
+      }
+    ]
+  }
+  
+  export default resumeData
